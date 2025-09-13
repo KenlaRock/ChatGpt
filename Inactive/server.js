e3204781b28ddd62a827017909ffbaa7f590d111
KenlaRock <148472687+KenlaRock@users.noreply.github.com>
2025-09-13 15:57:16 +0200

    Add rate limiting to inactive server

--- Inactive/server.js
+++ Inactive/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const path = require('path');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Configure rate limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
